7a4384bc543c728dd4d3ce3b9eacd7076d0c31ae
TheoLeanse <theoleanse@gmail.com>
2015-06-30 11:58:12 +0100

    tests scenario: I can filter links by tag

--- spec/features/creating_links_spec.rb
+++ spec/features/creating_links_spec.rb
@@ -1,4 +1,5 @@
 feature 'Creating Links' do
+
   scenario 'I can create a new link' do
     visit '/links/new'
     fill_in 'url',   with: 'http://www.zombo.com/'
@@ -11,4 +12,9 @@ feature 'Creating Links' do
       expect(page).to have_content('This is Zombocom')
     end
   end
+
+  scenario 'there are no links in the database at the start of the test' do
+    expect(Link.count).to eq 0
+  end
+
 end
