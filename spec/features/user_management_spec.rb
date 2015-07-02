477f39ca86602af160689bf9537004aaa446c680
MassimilianoMura <massimilianomura1983@gmail.com>
2015-07-02 15:31:49 +0100

    created test to check if user can sign in with correct credentials

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -39,4 +39,33 @@ feature 'User sign up' do
     fill_in :password_confirmation, with: password_confirmation
     click_button 'Sign up'
   end
+
 end
+
+feature 'User sign in' do
+
+  scenario 'with correct credentials' do
+    user = create :user
+    sign_in(user.email, user.password)
+    expect(page).to have_content "Welcome, #{user.email}"
+  end
+
+  def sign_in(email, password)
+    visit '/sessions/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_button 'Sign in'
+  end
+
+
+end
+
+
+
+
+
+
+
+
+
+
